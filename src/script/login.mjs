2e8c86bfba72322c60982431b6b0a88c011381b7
amaro silva <amarosilvasouza1@gmail.com>
2024-02-11 17:40:17 -0300

    atulizando

--- src/script/login.mjs
+++ src/script/login.mjs
@@ -8,11 +8,10 @@ const clikEnter = () => {
 }
 
 const inputAnalysisUser = () => {
-    const password1 = document.querySelector(".password_input")
-    const email1 = document.querySelector(".email_input")
+    const password = document.querySelector(".password_input").value
+    const email = document.querySelector(".email_input").value
 
-    const password = password1.value
-    const email = email1.value
+    console.log(password, email)
 
     //chame sua funçao na linha a baixo, a funçao que vai analiza a senha e email, passando o parametro como (password e email) ex: funcion (password, email)
 
--- src/script/register.mjs
+++ src/script/register.mjs
@@ -13,22 +13,19 @@ const inputAnalysisUser = () => {
     const repeatPassword = document.querySelector(".password_repeat_input").value
     const name = document.querySelector(".name_input").value
 
-    
     console.log(password,email,repeatPassword,name)
+
     // chame sua funçao abaixo passando os paramentos de (password, email, reptyPasword , name)
+    
 }
+
+clikEnter()
 
 // faça uma funçao que resebe (password, email, reptyPasword , name) e addiciona un novo usuario a {dataUser} que esta em data.mjs que esta importado na linha 1
 // DICA: nao se esqueca de definir o acceso do usuario como (false)
 // faça sua logica a parti da qui 
 
-
-
-
 // na sua funçao verifique se ja tem um usuario com o mesmo nome ou email em dataUser e se ja estiver retone que o usuario ja posui uma conta criada 
 // se nao, colete todos os paramentos e adicione ao array em dadaUser
 // ao final retone que o usuario conseguiu se registra com suseco
 // DICA: nao se esqueca de definir o acceso do usuario como (false)
-
-clikEnter()
-
